8aa7efe36a9c138593ddbf7a223b6cfbe1ea770b
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2022-09-18 22:49:59 +0300

    [FIX] tests: there is no need to check existed of path on createTempDir
    It will be checked automatically by mkdtemp function

--- source/thepath.d
+++ source/thepath.d
@@ -28,9 +28,6 @@ string createTempDirectory(in string prefix="tmp") {
 
 /// ditto
 string createTempDirectory(in string path, in string prefix) {
-    enforce!PathException(
-        std.file.exists(path),
-        "Path provided for 'createTempDirectory' does not exists");
     version(Posix) {
         import std.string : fromStringz;
         import std.conv: to;
@@ -49,7 +46,6 @@ string createTempDirectory(in string path, in string prefix) {
         enforce!PathException(
             res !is null, "Cannot create temporary directory");
 
-
         // Converting to string will duplicate result.
         // But may be it have sense to do it in more obvious way
         // for example: return tempname_str[0..$-1].idup;
@@ -58,13 +54,14 @@ string createTempDirectory(in string path, in string prefix) {
         import std.ascii: letters;
         import std.random: uniform;
 
+        // Generate new random temp path to test using provided path and prefix
+        // as template.
         string generate_temp_dir() {
-            string suffix = "";
+            string suffix = "-";
             for(ubyte i; i<6; i++) suffix ~= letters[uniform(0, $)];
             return std.path.buildNormalizedPath(
                 std.path.expandTilde(path), prefix ~ suffix);
         }
-
 
         string temp_dir = generate_temp_dir();
         while (std.file.exists(temp_dir)) {
