d47e218699e231e5ce2b1f5398845f24bc0aad71
Dmytro Katyukha <dmytro.katyukha@gmail.com>
2026-02-23 23:43:18 +0200

    Added support for cross-device moves

--- source/thepath/package.d
+++ source/thepath/package.d
@@ -9,7 +9,7 @@
   **/
 module thepath;
 
-public import thepath.path: Path;
+public import thepath.path: Path, CrossDeviceMove;
 public import thepath.utils: createTempDirectory, createTempPath;
 public import thepath.exception: PathException;
 
